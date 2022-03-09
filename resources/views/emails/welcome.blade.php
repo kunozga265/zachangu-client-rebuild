6e722a2d30882206b6b1e363fcda66384bb9c964
kunozga265 <kunozgamlowoka@gmail.com>
2022-03-09 18:02:38 +0200

    Welcome Mail

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Zachangu Online Loans</title>
+</head>
+<body>
+<div>
+    <p>Hi, {{$user->firstName}} {{$user->lastName}}!</p>
+    <br>
+    <p>Zachangu Microfinance Agency is pleased to welcome you. We offer a platform that allows individuals with formal employment access to quick payday loans of interest up to 10%.</p>
+    <p>Ensure your employer has submitted your details to our administrator. Apply for a loan or guarantee one today!</p>
+    <br>
+    <p>Zachangu Loans Administrator</p>
+</div>
+</body>
+</html>
